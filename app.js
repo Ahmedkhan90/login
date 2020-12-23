ba8e31b2e3414943c9a98be3f9ce1627de86f3ad
jawariaalvi32 <jawariaalvi32@gmail.com>
2020-12-23 18:03:36 +0500

    hi

--- app.js
+++ app.js
@@ -11,16 +11,16 @@
 //     }
 // }
 
-function signup(){
-var names = document.getElementById("names").value
-var email = document.getElementById("email").value
-var password = document.getElementById("password").value
+function signup() {
+    var names = document.getElementById("names").value
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
 }
- 
-var user ={
-    name: names ,
-     email: emails ,
-     password: passwords ,
+
+var user = {
+    name: names,
+    email: emails,
+    password: passwords,
 }
 
 function post() {
@@ -32,9 +32,62 @@ function post() {
     Http.send(JSON.stringify({ name: "M Ahmed Khan", time: "2pm" }));
 
     Http.onreadystatechange = (e) => {
-       console.log(Http.responseText)
+        console.log(Http.responseText)
     }
 }
+
+var emails1 = JSON.parse(localStorage.getItem("email"))
+var passwords1 = JSON.parse(localStorage.getItem("password"))
+function login() {
+
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let isFound = false;
+
+
+    for (let i = 0; i < emails1.length; i++) {
+
+        if (email === emails1[i]) {
+            isFound = i;
+            break;
+        }
+    }
+
+    if (isFound === false) {
+        alert("user not found")
+    }
+    else if (passwords1[isFound] === password) {
+
+        alert("login success");
+        window.location.href = "dashboard.html"
+
+    } else {
+        alert("Error Password or Username")
+    }
+
+
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function put() {
 //     const Http = new XMLHttpRequest();
 //     const url = 'http://192.168.50.177:3000/bulb';
